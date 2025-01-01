f6fc3c0c3d6d66a72e2aaadfb9eac3ff507829e1
Mithun021 <mithunkr79038@gmail.com>
2025-01-01 16:55:21 +0530

    setup home page

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,223 +1,113 @@
- <!-- Footer Area Start Here -->
- <footer>
-    <div class="footer-area-top">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
-                    <div class="footer-box">
-                        <a href="#"><img class="img-responsive" src="<?= base_url() ?>public/assets/image/logo.png" alt="logo" height="40px" /></a>
-                        <div class="footer-about">
-                            <p>
-                                NIPER was established through the Central University
-                                Act, 2009. The University started with a vision to specially focus on relevant
-                                present age educational drives with an emphasis on research in cutting edge
-                                technologies.
-                            </p>
+<!-- content close -->
+<a href="#" id="back-to-top"></a>
+        <!-- footer begin -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="widget">
+                            <a href="index.html"><img alt="" class="img-fluid mb20" src="images/logo-light.png"></a>
+                            <address class="s1">
+                                <span><i class="id-color fa fa-map-marker fa-lg"></i>08 W 36th St, New York, NY 10001</span>
+                                <span><i class="id-color fa fa-phone fa-lg"></i>+1 333 9296</span>
+                                <span><i class="id-color fa fa-envelope-o fa-lg"></i><a href="mailto:contact@example.com">contact@example.com</a></span>
+                                <span><i class="id-color fa fa-file-pdf-o fa-lg"></i><a href="#">Download Brochure</a></span>
+                            </address>
                         </div>
-                        <ul class="footer-social">
-                            <li>
-                                <a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                            </li>
-                            <li>
-                                <a href="https://twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                            </li>
+                    </div>
+                    <div class="col-md-4">
+                        <h5 class="id-color mb20">Practice Areas</h5>
+                        <ul class="ul-style-2">
+                            <li>Corporate and M&A</li>
+                            <li>Construction and Real Estate</li>
+                            <li>Commercial Duspute Resolution</li>
+                            <li>Employment</li>
+                            <li>Banking and Finance</li>
                         </ul>
                     </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
-                    <div class="footer-box">
-                        <h3>Featured Links</h3>
-                        <ul class="featured-links">
-                            <li>
-                                <ul>
-                                    <li><a href="">RTI</a></li>
-									
-									 <li><a href="">Act, Statutes, Ordinance</a></li>
-									 
-									 
-									 <li><a href="">Disclosure (Minutes of AC/EC/FC)</a></li>
-                                     <li> <a href=""><strong> Holidays - 2024</strong> </a> - <a href="file/CUJ notification no. 924 dated 13.12.2022 regarding list of holidays during the year 2023.pdf"><strong> 2023</strong></a></li>
-                                     <li><a href="">Download Forms</a></li>
-                                                                     
-                                    <li><a href="">Eminent Personalities</a></li>
-                                    											 
-                                </ul>
-                            </li>
-                        </ul>
+                    <div class="col-lg-4">
+                        <div class="widget">
+                            <h5 class="id-color">Newsletter</h5>
+                            <p>Signup for our newsletter to get the latest news, updates and special offers in your inbox.</p>
+                            <form action="blank.php" class="row" id="form_subscribe" method="post" name="form_subscribe">
+                                <div class="col text-center">
+                                    <input class="form-control" id="name_1" name="name_1" placeholder="enter your email" type="text" /> <a href="#" id="btn-submit"><i class="fa fa-long-arrow-right"></i></a>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </form>
+                            <div class="spacer-10"></div>
+                            <small>Your email is safe with us. We don't spam.</small>
+                        </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
-                    <div class="footer-box">
-                        <ul class="featured-links">
-                          <li>
-                            <ul>
-                              <li><a href="">MOOCS</a></li>
-                              <li><a href="">Swayam</a> --- <a href="https://swayamprabha.gov.in/">Swayam Prabha</a></li>
-                              <li><a href="https://www.digilocker.gov.in/">Digilocker</a></li>
-                              <li><a href="http://epgp.inflibnet.ac.in/">e-PG Pathshala</a></li>
-                              <li><a href="https://spoken-tutorial.org/">Spoken Tutorial</a></li>
-                              <li><a href="http://cuj.cuj.ac.in/vigilance.php">Vigilance</a></li>
-                            
-                              <li><a href="https://nptel.ac.in/">NPTEL</a></li>
-                              <li><a href="https://ndl.iitkgp.ac.in/">National Digital Library</a></li>
-                              <li><a href="https://www.ugc.ac.in/">UGC</a></li>
-                              <li><a href="https://www.education.gov.in/en/e-contents">MoE</a></li>
-                            </ul>
-                          </li>
-                        </ul>
-                        
-						
-						
-                  </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
-					
-					<h3>Contact Us</h3>
-                        <ul class="corporate-address">
-                            <li>
-                                <i class="fa fa-phone" aria-hidden="true"></i><a href="#"> + ___________</a>                            </li>
-                            <li>
-                                <i class="fa fa-envelope-o" aria-hidden="true"></i><a href="#" class="__cf_email__" data-cfemail="177e7971785775786463787972763974787a">___________</a>                            </li>
-                           <!-- <li><A href="http://cuj.cuj.ac.in/cujcontactus.php">Contact Us</li>-->
-                        </ul>
-                        <div class="newsletter-area">
-                           <!-- <h3><a href="newsletter.php">Newsletter</a></h3>  -->
-                            <div class="input-group stylish-input-group">
-                                <input type="text" placeholder="Enter your e-mail here" class="form-control" />
-                                <span class="input-group-addon">
-                                    <button type="submit">
-                                        <i class="fa fa-paper-plane" aria-hidden="true"></i>
-                                    </button>
-                                </span>
+            </div>
+            <div class="subfooter">
+                <div class="container">
+                    <div class="row g-4 align-items-center">
+                        <div class="col-lg-6">
+                            &copy; Copyright 2024 - Justica by Designesia
+                        </div>
+                        <div class="col-lg-6 text-lg-end">
+                            <div class="social-icons">
+                                <a href="#"><i class="fa fa-facebook fa-lg"></i></a>
+                                <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
+                                <a href="#"><i class="fa fa-linkedin fa-lg"></i></a>
+                                <a href="#"><i class="fa fa-pinterest fa-lg"></i></a>
+                                <a href="#"><i class="fa fa-rss fa-lg"></i></a>
                             </div>
                         </div>
-                  <!---<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7321.172612269053!2d85.25320609437595!3d23.43930676831683!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4dd97ba3aaaab%3A0xd680750f0fd2ab4d!2sCentral%20University%20of%20Jharkhand!5e0!3m2!1sen!2sin!4v1651689169073!5m2!1sen!2sin" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>--->
+                    </div>
                 </div>
+            </div>
+        </footer>
+        <!-- footer close -->
+        <div id="preloader">
+            <div class="spinner">
+                <div class="bounce1"></div>
+                <div class="bounce2"></div>
+                <div class="bounce3"></div>
             </div>
         </div>
     </div>
-    <div class="footer-area-bottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
-                    <p>
-                    &copy; 2024 NIPER All Rights Reserved. </p>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                    <!--<ul class="payment-method">
-                  <li>
-                    <a href="#"
-                      ><img alt="payment-method" src="img/payment-method1.jpg"
-                    /></a>
-                  </li>
-                  <li>
-                    <a href="#"
-                      ><img alt="payment-method" src="img/payment-method2.jpg"
-                    /></a>
-                  </li>
-                  <li>
-                    <a href="#"
-                      ><img alt="payment-method" src="img/payment-method3.jpg"
-                    /></a>
-                  </li>
-                  <li>
-                    <a href="#"
-                      ><img alt="payment-method" src="img/payment-method4.jpg"
-                    /></a>
-                  </li>
-                </ul>--->
-                </div>
-            </div>
+    <div id="selector">
+        <div id="demo-rtl" class="sc-opt">
+            <div class="sc-icon">RTL</div><span class="sc-val">Click to Enable</span>
+        </div>
+        <div class="sc-opt sc-mt">
+            <div class="sc-icon"><i class="fa fa-eyedropper"></i></div>
+            <span class="opt tc1" data-color="scheme-01"></span>
+            <span class="opt tc2" data-color="scheme-02"></span>
+            <span class="opt tc3" data-color="scheme-03"></span>
+            <span class="opt tc4" data-color="scheme-04"></span>
+            <span class="opt tc5" data-color="scheme-05"></span>
+            <span class="opt tc6" data-color="scheme-06"></span>
+            <span class="opt tc7" data-color="scheme-07"></span>
         </div>
     </div>
-</footer>        <!-- Footer Area End Here -->
-
-        <!--
-    <div class="modal fade" id="admission-popup" tabindex="-1" aria-hidden="false" style="top:10%;">
-        <div class="modal-dialog container">
-            <div class="modal-content p-0">
-                <div class="modal-body p-0">
-                    <img src="./img/popupCENTRAL-UNIVERSITY-OF-JHARK.jpg" style="width: 100%" />
-                </div>
-            </div>
-        </div>
+    <div id="purchase-now">
+        <a href="https://themeforest.net/cart/add_items?ref=designesia&item_ids=29485331"><span>$</span>25</a>
+        <div class="pn-hover">Buy Now</div>
     </div>
---->
-
-        <!-- jquery-->
-        <script src="<?= base_url() ?>public/assets/js/jquery-2.2.4.min.js" type="text/javascript"></script>
-        <!-- Plugins js -->
-        <script src="<?= base_url() ?>public/assets/js/plugins.js" type="text/javascript"></script>
-        <!-- Bootstrap js -->
-        <script src="<?= base_url() ?>public/assets/js/bootstrap.min.js" type="text/javascript"></script>
-        <!-- WOW JS -->
-        <script src="<?= base_url() ?>public/assets/js/wow.min.js"></script>
-        <!-- Nivo slider js -->
-        <script src="<?= base_url() ?>public/assets/vendor/slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
-        <script src="<?= base_url() ?>public/assets/vendor/slider/home.js" type="text/javascript"></script>
-        <!-- Owl Cauosel JS -->
-        <script src="<?= base_url() ?>public/assets/vendor/OwlCarousel/owl.carousel.min.js" type="text/javascript"></script>
-        <!-- Meanmenu Js -->
-        <script src="<?= base_url() ?>public/assets/js/jquery.meanmenu.min.js" type="text/javascript"></script>
-        <!-- Srollup js -->
-        <script src="<?= base_url() ?>public/assets/js/jquery.scrollUp.min.js" type="text/javascript"></script>
-        <!-- jquery.counterup js -->
-        <script src="<?= base_url() ?>public/assets/js/jquery.counterup.min.js"></script>
-        <script src="<?= base_url() ?>public/assets/js/waypoints.min.js"></script>
-        <!-- Countdown js -->
-        <script src="<?= base_url() ?>public/assets/js/jquery.countdown.min.js" type="text/javascript"></script>
-        <!-- Isotope js -->
-        <script src="<?= base_url() ?>public/assets/js/isotope.pkgd.min.js" type="text/javascript"></script>
-        <!-- Magic Popup js -->
-        <script src="<?= base_url() ?>public/assets/js/jquery.magnific-popup.min.js" type="text/javascript"></script>
-        <!-- Gridrotator js -->
-        <script src="<?= base_url() ?>public/assets/js/jquery.gridrotator.js" type="text/javascript"></script>
-        <!-- Custom Js -->
-        <script src="<?= base_url() ?>public/assets/js/main.js" type="text/javascript"></script>
-        <script src="<?= base_url() ?>public/assets/js/jquery.easing.min.js"></script>
-        <script src="<?= base_url() ?>public/assets/js/jquery.easy-ticker.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-       <script>
-            $(function () {
-                window.onload = () => {
-                    $("#admission-popup").modal("show");
-                };
-                // $(".slide-new").easyTicker({
-                //   direction: "up",
-                //   easing: "swing",
-                // });
-
-                $(".slide-events").easyTicker({
-                    visible: 2,
-                    interval: 4000,
-                    direction: "up",
-                    easing: "swing",
-                });
-
-                $(".slide-new").easyTicker({
-                    direction: "up",
-                    visible: 3,
-                    interval: 2500,
-                    controls: {
-                        up: ".btnUp",
-                        down: ".btnDown",
-                        toggle: ".btnToggle",
-                    },
-                });
-            });
-        </script>
+    <!-- Javascript Files
+    ================================================== -->
+    <script src="<?= base_url() ?>public/assets/js/jquery.min.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/bootstrap.min.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/wow.min.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/jquery.isotope.min.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/easing.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/owl.carousel.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/validation.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/enquire.min.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/jquery.stellar.min.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/jquery.plugin.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/typed.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/jarallax.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/jquery.countTo.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/jquery.countdown.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/typed.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/jarallax.js"></script>
+    <script src="<?= base_url() ?>public/assets/js/designesia.js"></script>
 </body>
 
 </html>
