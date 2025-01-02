da4b0f2c3da75ebea275a7c82e8d351c07f26e80
Mithun021 <mithunkr79038@gmail.com>
2025-01-02 12:05:34 +0530

    setup home page

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/codeigniter/search_lawyer/';
+    public string $baseURL = 'https://kanoonisahayata.in/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
